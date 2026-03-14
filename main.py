c15b550be89aae324b9915043fc9cd0ebc1db723
H-Burnside <h4nkburns08@gmail.com>
2026-03-14 07:42:38 -0300

    It remains the error

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 # author: Henrry Burnside 
 
 import os
+import pathlib
 
+destinydir = ''
+sourcedir = ''
 extensions = {
         "image": [".jpg",".jpeg",".png",".gif"],
-        "doc": [".pdf",".docx",".txt",".xlsx"]],
-        "video": [".mp4",".mov",".avi",".mkv"]],
+        "doc": [".pdf",".docx",".txt",".xlsx"],
+        "video": [".mp4",".mov",".avi",".mkv"],
         "audio": [".mp3",".wav"]
 }
 
@@ -19,6 +22,17 @@ def movefiles(path):
                 if file.is_dir:
                     movefiles(file)
                 elif file.is_file:
-                    
-    
+                    pass
 
+def movetodirectory(file):
+    isAnRequiredFile = False
+    extfile = file[os.extsep:]
+    for ext in extensions.keys():
+        if extfile in ext:
+            isAnRequiredFile = True
+            break
+    if(isAnRequiredFile):
+        destDir = f"{destinydir}{os.sep}{}"
+        if(!pathlib.Path(destDir).exists):
+            
+    
